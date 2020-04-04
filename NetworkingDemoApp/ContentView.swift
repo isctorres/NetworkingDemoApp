dbf8cfe22c0f9cea3a32b7275d6f44ad96fa6034
isctorres <isctorres@gmail.com>
2020-04-04 11:44:39 -0600

    Initial Commit

---
+++ NetworkingDemoApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NetworkingDemoApp
+//
+//  Created by Isc. Torres on 4/4/20.
+//  Copyright © 2020 isctorres. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
